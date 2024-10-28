e46680340addedfee836d9e06bbc21e4c531c209
Toni Daada <tonidaadaadagmail.com>
2024-10-28 15:32:25 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const btnRoll = document.querySelector('.btn--roll');
+const scorePlayerOne = document.getElementById('current--0');
+let currentScore = 0;
 
 // Setting the default elements of this items to 0
 score0Element.textContent = 0;
@@ -17,16 +19,23 @@ diceElement.classList.add('hidden');
 btnRoll.addEventListener('click', function () {
   // 1. Generate a random number
   const randomDiceRoll = Math.trunc(Math.random() * 6) + 1;
-  console.log(randomDiceRoll);
+  //   console.log(randomDiceRoll);
   // 2. Display dice roll
   diceElement.classList.remove('hidden');
   diceElement.src = `dice-${randomDiceRoll}.png`;
-
-  // 3. If its a 1 we shift to player two
-  if (randomDiceRoll == 1) {
-    console.log(`Player 2 turn`);
+  // 3.Check for if the roll is 1
+  if (randomDiceRoll !== 1) {
+    // 4. Add dice roll to current score
+    currentScore = currentScore + randomDiceRoll;
+    scorePlayerOne.textContent = currentScore;
+  } else {
+    // Shift two player two
+    currentScore = 0;
+    scorePlayerOne.textContent = currentScore;
   }
-  // 4. Add dice roll to current score
-
+});
+// Adding an event listener that will listen to when the hold button is clicked
+btnHold.addEventListener('click', function () {
   // 5. Display current score
+  score0Element.textContent = currentScore;
 });
