be7b104b703c91c93aae3b718113d9587695beb6
Toni Daada <tonidaadaadagmail.com>
2024-10-28 14:43:27 +0100

    modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+//I am selecting elements
+let score0Element = document.querySelector('#score--0');
+let score1Element = document.getElementById('score--1');
+let diceElement = document.querySelector('.dice');
+// Setting the default elements of this items to 0
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+// Now we want to hide the dice
+diceElement.classList.add('hidden');
